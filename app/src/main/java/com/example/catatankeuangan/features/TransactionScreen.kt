65bde743cacd2c53ca7227a12b01f85ecee44a1c
fndillah <affanfnl@gmail.com>
2023-11-20 12:46:35 +0700

    fix bug

--- app/src/main/java/com/example/catatankeuangan/features/TransactionScreen.kt
+++ app/src/main/java/com/example/catatankeuangan/features/TransactionScreen.kt
@@ -1,19 +1,17 @@
 package com.example.catatankeuangan.features
 
-import android.view.SurfaceControl.Transaction
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import java.util.UUID
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
+@Preview
 fun TransactionScreen() {
     // Mendapatkan instance ViewModel
-    val viewModel:  TransactionViewModel = ViewModel()
+    val viewModel:  TransactionViewModel
 
     // Membuat State untuk menyimpan deskripsi dan jumlah transaksi baru
     var description: String by rememberSaveable { mutableStateOf("") }
@@ -28,8 +26,8 @@ fun TransactionScreen() {
         onAddTransaction = {
             // Validasi dan tambahkan transaksi
             if (description.isNotEmpty() && amount.isNotEmpty()) {
-                Transaction (UUID.randomUUID().toString(), description, amount.toDouble())
-                viewModel.addTransaction(transaction)
+//                Transaction (UUID.randomUUID().toString(), description, amount.toDouble())
+//                viewModel.addTransaction(transaction)
                 // Mengosongkan input setelah transaksi ditambahkan
                 description = ""
                 amount = ""
@@ -42,6 +40,6 @@ fun TransactionScreen() {
     )
 
     // Menampilkan daftar transaksi dan total saldo
-    TransactionList(transactions = viewModel.transactions.collectAsState().value)
-    TotalBalance(totalBalance = viewModel.totalBalance.collectAsState().value)
+//    TransactionList(transactions = viewModel.transactions.collectAsState().value)
+//    TotalBalance(totalBalance = viewModel.totalBalance.collectAsState().value)
 }
